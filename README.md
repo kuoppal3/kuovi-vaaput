8022db6bebc2d5d035d52538419c8f6112025e3d
Ville Kuoppala <kuoppala@gmail.com>
2015-02-01 19:23:48 +0000

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## Vaappusivusto tilauskorilla
+Vaappusivusto, jossa asiakas voi kerätä vaappuja tilauskoriin ja suorittaa tilauksen.
+
+Toteutus: Node.js, Angular.js, Express.js, MongoDB, Three.js, Twitter Bootstrap 3
--- server.js
+++ server.js
@@ -54,11 +54,11 @@ app.get('/', routes.index);
 app.get('/tilaus', tilaus.tilaus);
 app.post('/tilaus', tilaus.lisaaTilaus);
 app.get('/tilaukset', tilaus.tilaukset);
-app.get('/yhteystiedot', yhteystiedot.yhteystiedot);
 app.get('/vaaput', vaaput.vaaput);
 app.get('/vaaput/50mm/varikartta', vaaput.varikartta50mm);
 app.get('/vaaput/120mm/3d', vaaput.vaappu3d);
 app.get('/kuvat', kuvia.kuvat);
+app.get('/yhteystiedot', yhteystiedot.yhteystiedot);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
